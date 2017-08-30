35d89aedee0ec6f210fb6d3a3287ade7ca4a302f
pawel.kamienski <pawel.kamienski@bit-sa.pl>
2017-08-30 08:19:32 +0200

    dodana domyslna akcja wywolujaca appke

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,6 +4,7 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.MobilePlatform;
+import io.appium.java_client.android.Activity;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
@@ -28,9 +29,11 @@ public class BaseTest {
         capabilities = DesiredCapabilities.android();
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "6.0.1"); // adb -s " + deviceID + " shell getprop ro.build.version.release
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "6.0.1"); // adb -s " + deviceID + " shell getprop ro.build.version.release //6.0.1 //4.4.4.
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android"); //wartosc ponoc nie uzywana ale wymagana
-        capabilities.setCapability(MobileCapabilityType.UDID, "YT910NB72F"); //ID zalezne od urzadzenia, pobiera sie z "adb devices"
+        capabilities.setCapability(MobileCapabilityType.UDID, "YT910NB72F"); //ID zalezne od urzadzenia, pobiera sie z "adb devices" pi: ZH80084532  moj:"YT910NB72F"
+        capabilities.setCapability("intentAction", "android.intent.action.MAIN"); // bez tego akcja bedzie nullem co moze powodowac problemy
+
         //File appDir = new File(userDir, fileName);
         //capabilities.setCapability(MobileCapabilityType.APP, appDir.getAbsolutePath());
         //Java package of the Android app you want to run.
@@ -40,10 +43,9 @@ public class BaseTest {
          * Activity name for the Android activity you want to launch from your package.
          * This often needs to be preceded by a . (e.g., .MainActivity instead of MainActivity).
          */
-        //capabilities.setCapability("appActivity", "pl.bitsa.lupe2.SplashActivity");
         capabilities.setCapability("appActivity", ".SplashActivity");
         //Activity name for the Android activity you want to wait for.
-        //capabilities.setCapability("appWaitActivity", ".LoginActivity");
+        capabilities.setCapability("appWaitActivity", ".LoginActivity");
         driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
 
     }
